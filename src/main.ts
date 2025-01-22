24fcb15e21f9c9fd02c73b21f6ae27a0395475ca
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-22 10:18:27 +0100

    refactor: Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  console.log(`serveurruning in : http://localhost:${process.env.PORT}`)
+  await app.listen(process.env.PORT ?? 6000);
 }
 bootstrap();
